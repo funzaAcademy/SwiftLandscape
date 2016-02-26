456d2740a4b474a05d0473df9df4892a1ca18d01
Sanjay Noronha <funza@sanjays-macbook-air.home>
2016-02-26 10:44:48 +0100

    First Version

---
+++ SwiftLandscape/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLandscape
+//
+//  Created by Sanjay Noronha on 2016/02/25.
+//  Copyright © 2016 funza Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
